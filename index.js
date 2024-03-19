2a14d8b5940e8e3c77ab92976ecf5538372a3f43
unknown <pablo.domiciano2019@gmail.com>
2024-03-18 21:34:11 -0300

    Criação do Somador

---
+++ index.js
@@ -0,0 +1,6 @@
+function somar() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 + num2;
+    document.getElementById('resultado').innerHTML = 'O resultado da soma é: ' + resultado;
+}
